36591d2c30abe40873ac4804ee55b23f26614e25
Katharine Berry <katharine@kathar.in>
2009-08-01 17:02:38 +0100

    Pisg module uses aliases from security module.

--- modules/pisg.py
+++ modules/pisg.py
@@ -1,5 +1,6 @@
 # Note that this module assumes use of a hacked pisg.
 
+from __future__ import with_statement
 import os
 import subprocess
 import threading
@@ -8,6 +9,7 @@ PISG_OUTPUT = "/home/katharine/web/irc.ajaxlife.net/stats/"
 PISG_WEB_URL = "http://irc.ajaxlife.net/stats/%s/%s.html"
 
 def run_pisg(network, channel, irc=None):
+    update_nicks()
     if irc is not None:
         m('irc_helpers').message(irc, channel, "Generating stats...")
     if not os.path.exists(PISG_OUTPUT+network):
@@ -23,6 +25,18 @@ def run_pisg(network, channel, irc=None):
     if irc is not None:
         m('irc_helpers').message(irc, channel, "Stats generation completed. You can see them at %s" % (PISG_WEB_URL % (network, channel[1:])))
 
+def update_nicks():
+    aliases = m('datastore').query("SELECT alias, canon FROM aliases")
+    nicks = {}
+    for alias in aliases:
+        if alias[1] not in nicks:
+            nicks[alias[1]] = StatUser(alias[1])
+        nicks[alias[1]].aliases.append(alias[0])
+    with open('data/pisg/nicks.cfg', 'w') as f:
+        for nick in nicks:
+            user = nicks[nick]
+            f.write('<user nick="%s" alias="%s">\n' % (nick, ' '.join(user.aliases)))
+
 def pisg(network, channel, irc=None):
     thread = threading.Thread(target=run_pisg, name="pisg-manual-%s-%s" % (network, channel), args=(network, channel), kwargs={'irc': irc})
     thread.start()
@@ -33,3 +47,12 @@ def init():
 def message(irc, channel, origin, command, args):
     if command == 'stats':
         pisg(irc.network.name, channel, irc)
+
+class StatUser(object):
+    aliases = None
+    nick = ''
+    gender = ''
+    
+    def __init__(self, nick):
+        self.nick = nick
+        self.aliases = []
